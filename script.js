a5a7f242e33771ff4bed82008b3cc5b6c5b04396
quispemamani0606-sys <quispemamani06.06@gmail.com>
2026-07-07 12:39:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let operacion = "";
+let operadores = ["+", "-", "*", "/", "="];
+
+while (true) {
+
+    let entrada = prompt();
+
+    if (!isNaN(entrada) || operadores.includes(entrada)) {
+
+        if (entrada === "=") {
+            break;
+        }
+
+        operacion += entrada;
+
+    } else {
+
+        console.log("Entrada no válida");
+
+    }
+
+}
+
+console.log("Operación:", operacion);
+
+let resultado = eval(operacion);
+
+console.log("Resultado:", resultado);
 
